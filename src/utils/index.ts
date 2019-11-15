4feb9dfd4aa1c635e4fa04759a7a47743470957b
Tom Trinca <tptrinca@gmail.com>
2019-11-15 14:43:15 +1100

    Move to utils index

---
+++ src/utils/index.ts
@@ -0,0 +1,21 @@
+import * as fs from "fs"
+import * as crypto from "crypto"
+
+export const doesFileExist = (filePath: string) => {
+  try {
+    if (fs.existsSync(filePath)) {
+      return true
+    }
+  } catch (err) {
+    // console.error(err)
+    return false
+  }
+}
+
+export const checksumFile = (filePath: string) => {
+  const data = fs.readFileSync(filePath)
+  return crypto
+    .createHash('md5')
+    .update(data)
+    .digest('hex')
+}
