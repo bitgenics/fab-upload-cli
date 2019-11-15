51d7d66e4feeb3fcca45de4138bd9b7ca15ed750
Tom Trinca <tptrinca@gmail.com>
2019-11-15 14:44:28 +1100

    Reference BuildStatus enum

--- src/utils/generateFab.ts
+++ src/utils/generateFab.ts
@@ -1,22 +1,28 @@
 const util = require("util");
+const { BuildStatus } = require("../enums")
+
 const exec = util.promisify(require("child_process").exec);
-const { BuildStatus } = require('../types')
 
 const DEV_COMMAND = "cd ~/Work/linc-front-end && yarn build:fab";
 const PROD_COMMAND = "yarn build:fab";
 
 const generateFab = async () => {
+  const started_at = Date.now()
   try {
     const { stdout } = await exec(DEV_COMMAND);
     return {
+      started_at,
+      finished_at: Date.now(),
+      status: BuildStatus.SUCCESS,
       log: stdout,
-      status: "success"
     };
   } catch (e) {
     const { stdout, stderr } = e;
     return {
+      started_at,
+      finished_at: Date.now(),
+      status: BuildStatus.FAILED,
       log: `${stdout}${stderr}`,
-      status: "failed"
     }
   }
 };
