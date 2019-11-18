1be3e85ee172fff7bb57602d4c505fff1ef764f9
Tom Trinca <tptrinca@gmail.com>
2019-11-18 16:02:47 +1100

    Add signed request status text enum

--- src/enums.ts
+++ src/enums.ts
@@ -1,1 +1,8 @@
 export enum BuildStatus { SUCCESS = "success", FAILED = "failed" }
+
+export enum SignedReqStatusText {
+  BUNDLE_EXISTS = "bundle_exists",
+  BUNDLE_UNIQUE = "bundle_unique",
+  NOT_AUTHORIZED = "not_authorized",
+  SERVER_ERROR = "server_error"
+}
