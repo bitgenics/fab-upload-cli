141eee7896d65ff549f5e1357658bacf2d63fbf8
Tom Trinca <tptrinca@gmail.com>
2019-11-18 16:04:20 +1100

    Add uploadMetadata method

---
+++ src/utils/uploadMetadata.ts
@@ -0,0 +1,28 @@
+import fetch from "node-fetch"
+import { CommitMetadata, BuildInfo } from "../types"
+
+const FAB_UPLOAD_ENDPOINT = "https://graphql.linc.sh//fab_upload/upload_metadata"
+const DEV_ENDPOINT = "http://localhost:3001/fab_upload/upload_metadata"
+
+type MetadataPayload = {
+  sitename: string,
+  api_key: string,
+  bundle_id?: string,
+  commit_info: CommitMetadata,
+  build_info: BuildInfo,
+}
+
+const uploadMetadata = async (metadataPayload: MetadataPayload) => {
+  const response = await fetch(DEV_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: "follow",
+    body: JSON.stringify(metadataPayload)
+  })
+  return await response.json()
+
+}
+
+export default uploadMetadata
