3d7929923620470d1867d58f3d337efefa7e4a65
Tom Trinca <tptrinca@gmail.com>
2019-11-19 16:45:57 +1100

    Hard code build:fab command

--- src/utils/generateFab.ts
+++ src/utils/generateFab.ts
@@ -3,13 +3,10 @@ const { BuildStatus } = require("../enums")
 
 const exec = util.promisify(require("child_process").exec);
 
-const DEV_COMMAND = "cd ~/Work/linc-front-end && yarn build:fab";
-const PROD_COMMAND = "yarn build:fab";
-
 const generateFab = async () => {
   const started_at = Date.now()
   try {
-    const { stdout } = await exec(DEV_COMMAND);
+    const { stdout } = await exec("npm run build:fab");
     return {
       started_at,
       finished_at: Date.now(),
