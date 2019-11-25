f32eef458f71daee952d4017da1c0780a90cbaab
Tom Trinca <tptrinca@gmail.com>
2019-11-25 16:28:30 +1100

    Define server errors enum

--- src/enums.ts
+++ src/enums.ts
@@ -1,8 +1,8 @@
 export enum BuildStatus { SUCCESS = "success", FAILED = "failed" }
 
-export enum StatusTextOptions {
-  DUPLICATE_BUNDLE = "duplicate_bundle",
-  NOT_AUTHORIZED = "not_authorized",
-  SERVER_ERROR = "server_error",
-  OK = "ok",
+export enum Errors {
+  NOT_AUTHED = "not_authed",
+  FATAL_ERROR = "fatal_error",
+  INVALID_SITE_NAME = "invalid_site_name",
+  INVALID_ARGUMENTS = "invalid_arguments",
 }
