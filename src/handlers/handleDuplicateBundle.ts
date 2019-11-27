dc9fe96680132a198b1cbf5da47b2dd77bf65d8e
Tom Trinca <tptrinca@gmail.com>
2019-11-27 14:54:57 +1100

    Handle preview URLs on duplicate bundle

--- src/handlers/handleDuplicateBundle.ts
+++ src/handlers/handleDuplicateBundle.ts
@@ -1,7 +1,10 @@
-import uploadMetadata from "../utils/uploadMetadata"
-import { getGitData } from "../utils/git"
-import { logUrls } from "../utils"
 import { CommitMetadata, BuildInfo, BundleInfo } from "../types"
+import { logUrls, composeLincCommitPageUrl } from "../utils"
+import { getGitData } from "../utils/git"
+import { log } from "../utils/log"
+import uploadMetadata from "../utils/uploadMetadata"
+
+import handleServerError from "./handleServerError"
 
 const duplicateBundle = async (sitename: string, api_key: string, bundle_info: BundleInfo, buildInfo: BuildInfo) => {
   const gitMetaData: CommitMetadata = await getGitData()
@@ -12,7 +15,18 @@ const duplicateBundle = async (sitename: string, api_key: string, bundle_info: B
     commit_info: gitMetaData,
     build_info: buildInfo,
   })
-  logUrls(response.urls)
+  if (response.ok) {
+    log("Done!")
+    const { preview_urls } = response
+    if (preview_urls) {
+      log("FAB preview URLs:")
+      logUrls(response.preview_urls)
+      log("View commit on Linc:")
+      composeLincCommitPageUrl(sitename, gitMetaData.commitHash)
+    }
+  } else {
+    handleServerError(response.error)
+  }
 }
 
 export default duplicateBundle
