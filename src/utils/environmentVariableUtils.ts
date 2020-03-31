b34ff95bd16aa96c571b784f60279b6646e52d58
Tom Trinca <tptrinca@gmail.com>
2020-03-31 14:15:58 +1100

    Add logic to extract Bamboo specific environment variables

--- src/utils/environmentVariableUtils.ts
+++ src/utils/environmentVariableUtils.ts
@@ -38,23 +38,31 @@ export const getCI = () => {
   Returns the current git branch for a provided CI platform name.
 */
 export const getBranch = (platform: string) => {
-  const { TRAVIS_BRANCH, BUILDKITE_BRANCH, CIRCLE_BRANCH, CI_COMMIT_BRANCH } = process.env
+  const FALLBACK = "unknown-branch"
 
   switch (platform) {
     case PLATFORMS.TRAVIS:
-      return TRAVIS_BRANCH // note: this can evaulate to undefined....
+      const { TRAVIS_BRANCH } = process.env
+      return TRAVIS_BRANCH || FALLBACK
 
     case PLATFORMS.BUILDKITE:
-      return BUILDKITE_BRANCH
+      const { BUILDKITE_BRANCH } = process.env
+      return BUILDKITE_BRANCH || FALLBACK
 
     case PLATFORMS.CIRCLECI:
-      return CIRCLE_BRANCH
+      const { CIRCLE_BRANCH } = process.env
+      return CIRCLE_BRANCH || FALLBACK
 
     case PLATFORMS.GITLAB:
-      return CI_COMMIT_BRANCH
+      const { CI_COMMIT_BRANCH } = process.env
+      return CI_COMMIT_BRANCH || FALLBACK
+
+    case PLATFORMS.BAMBOO:
+      const { bamboo_planRepository_branch } = process.env
+      return bamboo_planRepository_branch || FALLBACK
 
     default:
-      return "unkown-branch"
+      return FALLBACK
   }
 }
 
@@ -62,27 +70,36 @@ export const getBranch = (platform: string) => {
   Returns the current git repo for a provided CI platform name.
 */
 export const getRepo = (platform: string) => {
-  const {
-    TRAVIS_REPO_SLUG,
-    BUILDKITE_REPO,
-    CIRCLE_PROJECT_REPONAME,
-    CI_PROJECT_PATH
-  } = process.env
+  const FALLBACK = "unknown-repo"
 
   switch (platform) {
     case PLATFORMS.TRAVIS:
-      return TRAVIS_REPO_SLUG
+      const { TRAVIS_REPO_SLUG } = process.env
+      return TRAVIS_REPO_SLUG || FALLBACK
 
     case PLATFORMS.BUILDKITE:
-      return BUILDKITE_REPO
+      const { BUILDKITE_REPO } = process.env
+      return BUILDKITE_REPO || FALLBACK
 
     case PLATFORMS.CIRCLECI:
-      return CIRCLE_PROJECT_REPONAME
+      const { CIRCLE_PROJECT_REPONAME } = process.env
+      return CIRCLE_PROJECT_REPONAME || FALLBACK
 
     case PLATFORMS.GITLAB:
-      return CI_PROJECT_PATH
+      const { CI_PROJECT_PATH } = process.env
+      return CI_PROJECT_PATH || FALLBACK
+
+    case PLATFORMS.BAMBOO:
+      const repoUrl = process.env.bamboo_planRepository_repositoryUrl
+      if (repoUrl) {
+        const splitPoint = "https://bitbucket.org/"
+        const splitUrl = repoUrl.split(splitPoint)
+        if (splitUrl && splitUrl.length > 1 && splitUrl[1].length > 0) {
+          return splitUrl[1]
+        }
+      }
 
     default:
-      return "unknown-repo"
+      return FALLBACK
   }
 }
