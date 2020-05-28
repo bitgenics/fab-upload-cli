7cbf5380b07aaadab984eb43d86827b70f19a8d0
Tom Trinca <tptrinca@gmail.com>
2020-05-28 14:27:46 +1000

    Destructure chalk colors

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,13 +1,13 @@
-import * as chalk from 'chalk'
+import { gray, red, yellow, green } from "chalk"
 
 const PROJECT_NAME = `[fab-upload]`
 const _log = (colour: (str: string) => string) => (str: string) =>
   console.log(
-    `${chalk.gray(PROJECT_NAME)} ${colour(
+    `${gray(PROJECT_NAME)} ${colour(
       str.split(/\n\s*/).join(`\n${PROJECT_NAME.replace(/./g, ' ')} `)
     )}`
   )
 export const log = _log(str => str)
-export const note = _log(chalk.yellow)
-export const good = _log(chalk.green)
-export const error = _log(chalk.red)
+export const note = _log(yellow)
+export const good = _log(green)
+export const error = _log(red)
